dc57ad7d862dbe527f4dc95eba6c5761294ac79c
Nelson Coelho <nelson@inovadora.com.br>
2024-04-30 10:13:31 -0300

    Melhorias na rotina da pesquisa fonetica

--- db/migrate/20240420181132_add_extencoes_pesquisa.rb
+++ db/migrate/20240420181132_add_extencoes_pesquisa.rb
@@ -1,7 +1,8 @@
 class AddExtencoesPesquisa < ActiveRecord::Migration[7.0]
   def change
-    enable_extension 'pg_trgm'
-    enable_extension 'fuzzystrmatch'
+    #enable_extension 'pg_trgm'
+    #enable_extension 'fuzzystrmatch'
+    #deu erro de permissão, então habilitei pelo psql mesmo, entrando no banco
 
     ActiveRecord::Base.connection.execute("
     CREATE OR REPLACE FUNCTION soundex_individual_names(full_name TEXT)
